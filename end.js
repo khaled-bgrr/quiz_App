3f06103356cd7dd73cb54bf288b81bd0a3c78ccb
Khaled <bougherrakhaled@gmail.com>
2020-05-23 07:46:05 +0100

    made hud more cleaner

---
+++ end.js
@@ -0,0 +1,14 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+const finalScore =document.getElementById("finalScore");
+finalScore.innerText = mostRecentScore;
+
+
+username.addEventListener('keyup',()=>{
+    saveScoreBtn.disabled = !username.value;
+})
+
+
+
+
